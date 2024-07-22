2d43cc44615614e30f42881e486a1b2d230fc5b7
always1ov <84728964+always1ov@users.noreply.github.com>
2024-07-22 15:22:23 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "always1ovの云盘",
+  title: "ALWAYS1OVの网盘",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -38,11 +38,11 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<a href="https://www.bing.com/" target="_blank" rel="noopener noreferrer">always1ov</a>',
+    'Copyright by <a href="https://www.bing.com/" target="_blank" rel="noopener noreferrer">always1ov</a><br>本站为私人网盘，网盘资源仅供学习交流，请勿用于商业用途，下载请于24小时内删除。',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/Persional'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
